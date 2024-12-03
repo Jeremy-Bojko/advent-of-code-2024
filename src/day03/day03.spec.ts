4532b6d483e479de6dab2abec9d98447683bdb0a
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-12-03 10:07:25 +0100

    [feat] day03 - part 2

--- src/day03/day03.spec.ts
+++ src/day03/day03.spec.ts
@@ -1,4 +1,4 @@
-import { checkInstruction, extractDigit } from "./main";
+import { checkInstruction, extractDigit, ignoreDontData } from "./main";
 
 describe("Day 3", () => {
   it("should return correct operation", () => {
@@ -53,5 +53,36 @@ describe("Day 3", () => {
     const instructionInput = "mul(42,8)";
     const instructionOutput = [42, 8];
     expect(extractDigit(instructionInput)).toEqual(instructionOutput);
+  });
+  it("should return correct operation", () => {
+    const instructionInput = "don't()";
+    const instructionOutput: string[] = ["don't()"];
+    expect(checkInstruction(instructionInput)).toEqual(instructionOutput);
+  });
+  it("should return correct operation", () => {
+    const instructionInput = "don't()dezdzdo()";
+    const instructionOutput: string[] = ["don't()", "do()"];
+    expect(checkInstruction(instructionInput)).toEqual(instructionOutput);
+  });
+  it("should return correct operation", () => {
+    const instructionInput = "don't()demul(42,8)zdzdo()";
+    const instructionOutput: string[] = ["don't()", "mul(42,8)", "do()"];
+    expect(checkInstruction(instructionInput)).toEqual(instructionOutput);
+  });
+  it("should return correct operation", () => {
+    const instructionInput = "mul(42,8)don't()demul(42,8)zdzdo()mul(42,8)";
+    const instructionOutput: string[] = ["mul(42,8)", "don't()", "mul(42,8)", "do()", "mul(42,8)"];
+    expect(checkInstruction(instructionInput)).toEqual(instructionOutput);
+  });
+  it("should ignore 'don't' instructions ", () => {
+    // const instructionInput = ["mul(42,8)", "don't()", "mul(42,8)", "do()", "mul(42,8)"];
+    const instructionInput = ["mul(42,8)", "don't()", "mul(42,8)", "mul(8,42)"];
+    const instructionOutput: string[] = ["mul(42,8)"];
+    expect(ignoreDontData(instructionInput)).toEqual(instructionOutput);
+  });
+  it("should ignore 'don't' instructions ", () => {
+    const instructionInput = ["mul(42,8)", "don't()", "mul(42,8)", "do()", "mul(42,842)"];
+    const instructionOutput: string[] = ["mul(42,8)", "do()", "mul(42,842)"];
+    expect(ignoreDontData(instructionInput)).toEqual(instructionOutput);
   });
 });
--- src/day03/main.ts
+++ src/day03/main.ts
@@ -6,14 +6,14 @@ const testData = `${__dirname}/testData.txt`;
  * Part 1
  */
 
-utilities.fetchAndTest(inputData).then((data: string) => {
-  const corruptedInstructions = data;
-  const correctInstruction = checkInstruction(corruptedInstructions).map((x) => extractDigit(x));
-  console.log(correctInstruction.map((x) => x[0] * x[1]).reduce((a, b) => a + b));
-});
+// utilities.fetchAndTest(inputData).then((data: string) => {
+//   const corruptedInstructions = data;
+//   const correctInstruction = checkInstruction(corruptedInstructions).map((x) => extractDigit(x));
+//   console.log(correctInstruction.map((x) => x[0] * x[1]).reduce((a, b) => a + b));
+// });
 
 export const checkInstruction = (instruction: string): string[] => {
-  const regex = /mul\((\d+),(\d+)\)/g;
+  const regex = /mul\((\d+),(\d+)\)|(don\'t\(\))|(do\(\))/g;
   const result = instruction.match(regex);
   return result ? result : [];
 };
@@ -27,3 +27,22 @@ export const extractDigit = (instruction: string): number[] => {
 /**
  * Part 2
  */
+
+utilities.fetchAndTest(inputData).then((data: string) => {
+  const corruptedInstructions = data;
+  const extractedInstructions = ignoreDontData(checkInstruction(corruptedInstructions));
+  const correctInstruction = extractedInstructions.filter((x) => x !== "do()").map((x) => extractDigit(x));
+  console.log(correctInstruction.map((x) => x[0] * x[1]).reduce((a, b) => a + b));
+});
+
+export const ignoreDontData = (data: string[]): string[] => {
+  let keepMode = true;
+  return data.filter((x) => {
+    if (x === "don't()") {
+      keepMode = false;
+    } else if (x === "do()") {
+      keepMode = true;
+    }
+    return keepMode;
+  });
+};
